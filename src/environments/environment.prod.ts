72525dfeb589a0e3e2c3a69d5a2e405063e734cd
Rémy Hidra <remy.hidra@gmail.com>
2020-11-07 11:44:19 +0100

    Update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,19 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.zuoyoubycurios.com/',
-  mediaHost: 'https://api.zuoyoubycurios.com',
+
+  // apiUrl: 'https://api.zuoyoubycurios.com/',
+  // mediaHost: 'https://api.zuoyoubycurios.com',
+  apiUrl: 'http://192.168.43.29:9000/',
+  mediaHost: 'http://192.168.43.29:9000',
+
+  firebase: {
+    apiKey: "AIzaSyB8ADk1r9qWGhMhg3--yZXeU001B9ned6M",
+    authDomain: "zuoyou-456ad.firebaseapp.com",
+    databaseURL: "https://zuoyou-456ad.firebaseio.com",
+    projectId: "zuoyou-456ad",
+    storageBucket: "zuoyou-456ad.appspot.com",
+    messagingSenderId: "373767670467",
+    appId: "1:373767670467:web:c46b9585fe805d758bb076",
+    measurementId: "G-KC5DJHGG8J",
+  },
 };
